ce6b71b1baf0b6ca77ee64e64d449197da631721
Quan Nguyen <quangineer@gmail.com>
2019-05-14 19:53:39 -0700

    lesson learnt

--- notes.py
+++ notes.py
@@ -23,3 +23,12 @@ for i in range(len(Seattle_temperature)):
         Seattle_temperature[i] = 0
         # 'nan' is  a string here. Thus to compare with it, we need 
         # to make a string for the other side of the equal comparison.
+
+#when using loop (for i in Varible), if we use Variable, it will 
+#create a new list separatedly from the list we loop. Be careful
+# when print this loop result as it may not show the changed list
+# (instead, it will show the unchanged statu quo list)
+
+#It is kind of sort and sorted when sort does not make a new list
+#  and only change within the given list. Otherwise, sorted make a 
+#  new list differentiating itself from the provided list.
