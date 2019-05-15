46319ce23d994dcf00791f5ac61116dbb818d365
Quan Nguyen <quangineer@gmail.com>
2019-05-14 20:51:01 -0700

    updates

--- main.py
+++ main.py
@@ -14,12 +14,27 @@ data2 = pd.read_csv(global_temperature)
 # Seattle_temperature = list((data1[data1.city == 'Seattle'].avg_temp).fillna(0))
 # print (Seattle_temperature)
 
-
 Seattle_temperature = list(data1[data1.city == 'Seattle'].avg_temp)
 for i in range(len(Seattle_temperature)):
     if str(Seattle_temperature[i]) == 'nan':
         Seattle_temperature[i] = 0
 
-print (Seattle_temperature)
+# print (Seattle_temperatu re)
 
+def running_mean(Seattle_temperature, N):
+    sum = 0
+    result = list( 0 for x in Seattle_temperature)
 
+    for i in range(0, N):
+        sum = sum + Seattle_temperature[i]
+        result[i] = sum / (i+1)
+    for i in range(N, len(Seattle_temperature)):
+        sum = sum - Seattle_temperature[i-N] + Seattle_temperature[i]
+        result[i] = sum/N
+    return result 
+
+# print(running_mean(Seattle_temperature, 7))
+A = running_mean(Seattle_temperature,7)
+
+moving_average = A[6:]
+print (moving_average)
